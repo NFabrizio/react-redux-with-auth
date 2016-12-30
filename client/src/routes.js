12cd3aa3e836fe82392a5d4d5388fa8d84f48e34
Nick Fabrizio <nfabrizio@medianewsgroup.com>
2016-12-29 22:52:58 -0700

    Adds base file structure

---
+++ client/src/routes.js
@@ -0,0 +1,23 @@
+import Base from './components/Base.jsx';
+import HomePage from './components/HomePage.jsx';
+import LoginPage from './containers/LoginPage.jsx';
+
+const routes = {
+  // base component (wrapper for the whole application).
+  component: Base,
+  childRoutes: [
+
+    {
+      path: '/',
+      component: HomePage
+    },
+
+    {
+      path: '/login',
+      component: LoginPage
+    },
+
+  ]
+};
+
+export default routes;
